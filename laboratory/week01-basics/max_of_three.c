1dd489b23fead34618a48676bb0ee1d758053709
AlenaAvramova <aavramova@tu-sofia.bg>
2026-05-01 17:23:56 +0300

    Add max_of_three.c to find largest of three numbers
    Implement program to find the maximum of three numbers

--- laboratory/week01-basics/max_of_three.c
+++ laboratory/week01-basics/max_of_three.c
@@ -1,1 +1,29 @@
 
+/*
+Program that reads three numbers and prints the largest
+of the three.
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int x, y, z;
+    int max;
+
+    printf("Enter 3 numbers: ");
+    scanf("%d %d %d", &x, &y,&z);
+
+    max = x;
+
+    if (y > max)
+        max = y;
+
+    if (z > max)
+        max = z;
+
+    printf("The largest number is: %d\n", max);
+
+    return 0;
+}
